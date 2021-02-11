e67171de4664d5356183036d46dc52dabc5225de
Victor de Oliveira <oliveira.wn@gmail.com>
2021-02-11 18:19:12 -0300

    primeiro commit

---
+++ src/modules/product/product.service.ts
@@ -0,0 +1,11 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { MySQLProvider } from 'src/database/mysql.provider';
+
+@Injectable()
+export class ProductService {
+  constructor(@Inject('DATABASE') private readonly mysql: MySQLProvider) {}
+  async findAll(): Promise<any> {
+    console.log(this.mysql.getValue());
+    return [{ id: 1 }, { id: 2 }, { id: 3 }];
+  }
+}
